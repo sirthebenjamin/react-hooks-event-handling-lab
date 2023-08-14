93d793e7a1d31d729a3cfb94492dea3bbc2002c5
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-08-13 22:44:23 -0400

    Adds myfile.md

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,66 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+  componentDidMount() {
+    // Add event listeners when the component is mounted
+    const button = document.getElementById('eyesButton');
+    const input = document.getElementById('eyesInput');
+    
+    button.addEventListener('focus', this.handleButtonFocus);
+    button.addEventListener('blur', this.handleButtonBlur);
+
+    input.addEventListener('focus', this.handleInputFocus);
+    input.addEventListener('blur', this.handleInputBlur);
+    input.addEventListener('input', this.handleInputChange);
+  }
+
+  componentWillUnmount() {
+    // Clean up by removing event listeners when the component is unmounted
+    const button = document.getElementById('eyesButton');
+    const input = document.getElementById('eyesInput');
+    
+    button.removeEventListener('focus', this.handleButtonFocus);
+    button.removeEventListener('blur', this.handleButtonBlur);
+
+    input.removeEventListener('focus', this.handleInputFocus);
+    input.removeEventListener('blur', this.handleInputBlur);
+    input.removeEventListener('input', this.handleInputChange);
+  }
+
+  handleButtonFocus = () => {
+    console.log('Good!');
+  }
+
+  handleButtonBlur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  handleInputFocus = () => {
+    console.log('Input in focus...');
+  }
+
+  handleInputBlur = () => {
+    console.log('Input lost focus...');
+  }
+
+  handleInputChange = (event) => {
+    console.log('Input value:', event.target.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <button id="eyesButton">
+          Eyes on me
+        </button>
+        <input
+          type="text"
+          id="eyesInput"
+          placeholder="Type here"
+        />
+      </div>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,31 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
 
-function Keypad (){
+class Keypad extends Component {
+  componentDidMount() {
+    // Add event listener when the component is mounted
+    document.getElementById('password').addEventListener('input', this.handleInputChange);
+  }
+
+  componentWillUnmount() {
+    // Clean up by removing the event listener when the component is unmounted
+    document.getElementById('password').removeEventListener('input', this.handleInputChange);
+  }
+
+  handleInputChange = (event) => {
+    console.log('Entering password...', event.target.value);
+  }
+
+  render() {
     return (
-        <div></div>
-    )
+      <div>
+        <label htmlFor="password">Enter Password: </label>
+        <input
+          type="password"
+          id="password"
+        />
+      </div>
+    );
+  }
 }
 
 export default Keypad;
